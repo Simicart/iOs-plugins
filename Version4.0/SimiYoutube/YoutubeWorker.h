aa90e8beabe7809e9855f770c45e6e33a2ccf0bd
Liam Đào <congnghiep.dao@trueplus.vn>
2016-12-01 16:48:15 +0700

    Update Youtube for second product detail

--- Version4.0/SimiYoutube/YoutubeWorker.h
+++ Version4.0/SimiYoutube/YoutubeWorker.h
@@ -6,7 +6,10 @@
 //  Copyright (c) 2015 Trueplus. All rights reserved.
 //
 
+static NSString *product_video_section = @"product_video_section";
+static NSString *product_video_row = @"product_video_row";
+
 #import <Foundation/Foundation.h>
-@interface YoutubeWorker : NSObject<UITableViewDataSource, UITableViewDelegate>
+@interface YoutubeWorker : NSObject<UITableViewDataSource, UITableViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
 @property (nonatomic, strong) UIButton* buttonSimiVideo;
 @end
