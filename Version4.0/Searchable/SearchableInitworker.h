326a7418030dab6a413dfd053f689af3751b010d
axehoang <axe@simicart.com>
2016-12-02 13:41:58 +0700

    update searchable plugin

---
+++ Version4.0/Searchable/SearchableInitworker.h
@@ -0,0 +1,13 @@
+//
+//  SearchableInitworker.h
+//  SimiCartPluginFW
+//
+//  Created by Hoang Van Trung on 12/1/16.
+//  Copyright © 2016 Trueplus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchableInitworker : NSObject
+
+@end
---
+++ Version4.0/Searchable/SearchableInitworker.m
@@ -0,0 +1,63 @@
+//
+//  SearchableInitworker.m
+//  SimiCartPluginFW
+//
+//  Created by Hoang Van Trung on 12/1/16.
+//  Copyright © 2016 Trueplus. All rights reserved.
+//
+
+#import "SearchableInitworker.h"
+#import <CoreSpotlight/CoreSpotlight.h>
+#import "SCThemeWorker.h"
+#import "SCProductViewController.h"
+
+@implementation SearchableInitworker
+
+-(id) init{
+    if(self == [super init]){
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didGetProducts:) name:@"DidGetProductCollection" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(openSearchableItem) name:@"DidInit" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(openSearchableItem) name:@"ContinueUserActivity" object:nil];
+    }
+    return self;
+}
+
+-(void) didGetProducts: (NSNotification*) noti{
+    NSArray* productList = noti.object;
+    if(productList.count > 0){
+        NSMutableArray* searchableItems = [[NSMutableArray alloc] initWithCapacity:0];
+        for(NSDictionary* product in productList){
+            NSUserActivity* userActivity = [[NSUserActivity alloc] initWithActivityType:[NSString stringWithFormat:@"%@.%@",[[NSBundle mainBundle] bundleIdentifier],[product objectForKey:@"entity_id"]]];
+            userActivity.title = [product objectForKey:@"name"];
+            userActivity.userInfo = @{@"id":[product objectForKey:@"entity_id"]};
+            userActivity.eligibleForSearch = YES;
+            CSSearchableItemAttributeSet* searchableItemAttributeSet = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:@"product"];
+            NSArray* images = [product objectForKey:@"images"];
+            if(images.count > 0)
+                searchableItemAttributeSet.thumbnailData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[[images objectAtIndex:0] objectForKey:@"url"]]];
+            searchableItemAttributeSet.title = [product objectForKey:@"name"];
+            searchableItemAttributeSet.contentDescription = [product objectForKey:@"short_description"];
+            id item = [[CSSearchableItem alloc]initWithUniqueIdentifier:[product objectForKey:@"entity_id"] domainIdentifier:[NSString stringWithFormat:@"%@.searchAPIs.product",[[NSBundle mainBundle] bundleIdentifier]] attributeSet:searchableItemAttributeSet];
+            [searchableItems addObject:item];
+            userActivity.contentAttributeSet = searchableItemAttributeSet;
+            [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:searchableItems completionHandler:^(NSError * _Nullable error) {
+                if(error != nil){
+                    NSLog(@"%@",error.localizedDescription);
+                }
+            }];
+        }
+    }
+}
+
+-(void) openSearchableItem{
+    NSUserActivity* searchableUserActivity = [SimiGlobalVar sharedInstance].searchableUserActivity;
+    if(searchableUserActivity){
+        SCProductViewController* productVC = [SCProductViewController new];
+        productVC.productId = [searchableUserActivity.userInfo objectForKey:@"kCSSearchableItemActivityIdentifier"];
+        [[[SimiGlobalVar sharedInstance] currentlyNavigationController] popToRootViewControllerAnimated:NO];
+        [[[SimiGlobalVar sharedInstance] currentlyNavigationController] pushViewController:productVC animated:YES];
+        searchableUserActivity = nil;
+    }
+}
+
+@end
