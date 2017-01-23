768ec5d9c4cd7c5d1552ef3f55c4d54922f2dd29
Hoang Van Trung <axe@simicart.com>
2017-01-23 13:27:14 +0700

    update from storelocator plugin

--- Version4.0/StoreLocator/SimiStoreLocatorWorker.m
+++ Version4.0/StoreLocator/SimiStoreLocatorWorker.m
@@ -14,9 +14,14 @@
 #import "SimiStoreLocatorViewControllerPad.h"
 #import <SimiCartBundle/SCNavigationBarPhone.h>
 #import <SimiCartBundle/SCNavigationBarPad.h>
+#import <SimiCartBundle/SCHomeViewController.h>
+
 @implementation SimiStoreLocatorWorker
 {
     SimiTable *cells;
+    BOOL isAddedButtonItem;
+    UIBarButtonItem* storeLocatorItem;
+    UIBarButtonItem* spacerItem;
 }
 
 - (instancetype)init
@@ -25,9 +30,49 @@
     if (self) {
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(initCellsAfter:) name:@"SCLeftMenu_InitCellsAfter" object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didSelectRow:) name:@"SCLeftMenu_DidSelectRow" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(addItemToLeftBar:) name:@"AddingLeftBarItems" object:nil];
     }
     [GMSServices provideAPIKey:@"AIzaSyAmBe73HHr9CU1lYU96CFg6PTwG2i6NDwU"];
     return self;
+}
+
+-(void) addItemToLeftBar:(NSNotification*) noti{
+    UINavigationController *currentVC = (UINavigationController*) [(UITabBarController *)[[(SCAppDelegate *)[[UIApplication sharedApplication] delegate] window] rootViewController] selectedViewController];
+    NSMutableArray* leftBarItems = noti.object;
+    if([currentVC.viewControllers.lastObject isKindOfClass:[SCHomeViewController class]]){
+        if(!isAddedButtonItem){
+            UIButton* storeLocatorButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
+            [storeLocatorButton setImage:[[UIImage imageNamed:@"sl_icon__09"] imageWithColor:THEME_NAVIGATION_ICON_COLOR] forState:UIControlStateNormal];
+            [storeLocatorButton addTarget:self action:@selector(openStoreLocatorView:) forControlEvents:UIControlEventTouchUpInside];
+            storeLocatorItem = [[UIBarButtonItem alloc] initWithCustomView:storeLocatorButton];
+            spacerItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+            spacerItem.width = 10;
+            [leftBarItems addObjectsFromArray:@[spacerItem, storeLocatorItem]];
+            isAddedButtonItem = YES;
+        }
+    }else{
+        if(isAddedButtonItem){
+            [leftBarItems removeObject:storeLocatorItem];
+            [leftBarItems removeObject:spacerItem];
+            isAddedButtonItem = NO;
+        }
+    }
+}
+
+-(void) openStoreLocatorView:(id) sender{
+    UINavigationController *currentVC = (UINavigationController*) [(UITabBarController *)[[(SCAppDelegate *)[[UIApplication sharedApplication] delegate] window] rootViewController] selectedViewController];
+    if (PADDEVICE) {
+        if(![currentVC.viewControllers.lastObject isKindOfClass:[SimiStoreLocatorViewControllerPad class]]){
+            SimiStoreLocatorViewControllerPad *storeLocatorViewController = [[SimiStoreLocatorViewControllerPad alloc]init];
+            [currentVC pushViewController:storeLocatorViewController animated:YES];
+        }
+    }else
+    {
+        if(![currentVC.viewControllers.lastObject isKindOfClass:[SimiStoreLocatorViewController class]]){
+            SimiStoreLocatorViewController *storeLocatorViewController = [[SimiStoreLocatorViewController alloc]init];
+            [currentVC pushViewController:storeLocatorViewController animated:YES];
+        }
+    }
 }
 
 - (void)initCellsAfter:(NSNotification*)noti
@@ -55,8 +100,7 @@
             UIViewController *currentVC = [(UITabBarController *)[[(SCAppDelegate *)[[UIApplication sharedApplication] delegate] window] rootViewController] selectedViewController];
             SimiStoreLocatorViewControllerPad *storeLocatorViewController = [[SimiStoreLocatorViewControllerPad alloc]init];
             [(UINavigationController *)currentVC pushViewController:storeLocatorViewController animated:YES];
-        }else
-        {
+        }else{
             UIViewController *currentVC = [(UITabBarController *)[[(SCAppDelegate *)[[UIApplication sharedApplication] delegate] window] rootViewController] selectedViewController];
             SimiStoreLocatorViewController *storeLocatorViewController = [[SimiStoreLocatorViewController alloc]init];
             [(UINavigationController *)currentVC pushViewController:storeLocatorViewController animated:YES];
