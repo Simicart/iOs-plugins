f85178e53058f721a0ce72396075016669f5aa2c
Liam Đào <congnghiep.dao@trueplus.vn>
2017-08-18 15:40:38 +0700

    Create Intercom Chat

---
+++ Version4.0/IntercomChat/SCIntercomWorker.h
@@ -0,0 +1,11 @@
+//
+//  SCIntercomWorker.h
+//  SimiCartPluginFW
+//
+//  Created by Liam on 12/21/16.
+//  Copyright © 2016 Trueplus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface SCIntercomWorker : NSObject
+@end
---
+++ Version4.0/IntercomChat/SCIntercomWorker.m
@@ -0,0 +1,39 @@
+//
+//  SCIntercomWorker.m
+//  SimiCartPluginFW
+//
+//  Created by Liam on 12/21/16.
+//  Copyright © 2016 Trueplus. All rights reserved.
+//
+
+#import "SCIntercomWorker.h"
+#import <Intercom/Intercom.h>
+@implementation SCIntercomWorker
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSString *intercomAppID = @"";
+        NSString *intercomiOsKey = @"";
+        
+        if ([[[SimiGlobalVar sharedInstance].allConfig valueForKey:@"intercom_chat"] valueForKey:@"intercom_app_id"]){
+            intercomAppID = [NSString stringWithFormat:@"%@",[[[SimiGlobalVar sharedInstance].allConfig valueForKey:@"intercom_chat"] valueForKey:@"intercom_app_id"]];
+        }
+        if ([[[SimiGlobalVar sharedInstance].allConfig valueForKey:@"intercom_chat"] valueForKey:@"intercom_ios_app_key"]) {
+            intercomiOsKey = [NSString stringWithFormat:@"%@",[[[SimiGlobalVar sharedInstance].allConfig valueForKey:@"intercom_chat"] valueForKey:@"intercom_ios_app_key"]];
+        }
+        if (![intercomiOsKey isEqualToString:@""] && ![intercomAppID isEqualToString:@""]) {
+            [Intercom setApiKey:intercomiOsKey forAppId:intercomAppID];
+            [Intercom setLauncherVisible:YES];
+            [Intercom registerUnidentifiedUser];
+        }
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+}
+@end
