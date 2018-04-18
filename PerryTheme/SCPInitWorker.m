acecbc59eb3426ab21e67a02cd0a80ff915d7448
Hoang Van Trung <axe@simicart.com>
2018-04-18 09:33:55 +0700

    update initialization

--- PerryTheme/SCPInitWorker.m
+++ PerryTheme/SCPInitWorker.m
@@ -7,7 +7,20 @@
 //
 
 #import "SCPInitWorker.h"
+#import <SimiCartBundle/InitWorker.h>
 
 @implementation SCPInitWorker
-
+- (id)init{
+    if(self == [super init]){
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(initializedRootController:) name:Simi_InitializedRootController object:nil];
+    }
+    return self;
+}
+- (void)initializedRootController:(NSNotification *)noti{
+    if([GLOBALVAR.appConfigModel objectForKey:@"‘perry_theme’"]){
+        InitWorker *initWorker = noti.object;
+        initWorker.isDiscontinue = YES;
+        //Init the root view
+    }
+}
 @end
