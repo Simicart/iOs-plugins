af29f91c43e0dc389caf4b648b5e6fde2f9ee303
Hoang Van Trung <axe@simicart.com>
2018-08-21 13:51:38 +0700

    update plugins

---
+++ Version4.0/StoreLocator/SimiStoreLocatorModel.h
@@ -0,0 +1,13 @@
+//
+//  SimiStoreLocatorModel.h
+//  SimiCartPluginFW
+//
+//  Created by Hoang Van Trung on 8/21/18.
+//  Copyright © 2018 Trueplus. All rights reserved.
+//
+
+#import <SimiCartBundle/SimiCartBundle.h>
+
+@interface SimiStoreLocatorModel : SimiModel
+
+@end
---
+++ Version4.0/StoreLocator/SimiStoreLocatorModel.m
@@ -0,0 +1,13 @@
+//
+//  SimiStoreLocatorModel.m
+//  SimiCartPluginFW
+//
+//  Created by Hoang Van Trung on 8/21/18.
+//  Copyright © 2018 Trueplus. All rights reserved.
+//
+
+#import "SimiStoreLocatorModel.h"
+
+@implementation SimiStoreLocatorModel
+
+@end
